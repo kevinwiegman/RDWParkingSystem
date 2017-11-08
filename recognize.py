68208012b93cf7f3e138e7db6989cff5cfe143da
BrandonKroes <b.kroes@protonmail.com>
2017-11-08 21:54:25 +0100

    Fixed static

--- recognize.py
+++ recognize.py
@@ -2,6 +2,6 @@ from PIL import Image
 import pytesseract
 
 def extract(file_path):
-    im = Image.open("example_03.png")
+    im = Image.open(file_path)
     text = pytesseract.image_to_string(im, lang='eng')
     return text
