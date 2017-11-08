1b082e9c3b4395d5085e6c80efe07bda38df53fb
Kevin Wiegman <kevin.wiegman@student.hu.nl>
2017-11-08 16:45:24 +0100

    EntryGUI V1.0 - Docstrings toegevoegd - Functions af

--- GUI/EntryGUI.py
+++ GUI/EntryGUI.py
@@ -2,16 +2,20 @@ from tkinter import *
 
 
 buttons = [
-    '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '',
+    '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', 'TAB',
     'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '\\', '7', '8', '9', 'BACK',
     'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '[', ']', '4', '5', '6'
     , 'SHIFT',
     'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '?', '/', '1', '2', '3', 'SPACE',
 ]
+
 invoer = []
 
 
 def select(value):
+    """"
+    Functie van de keyboard code, pakt de input en voegt deze toe aan list(invoer)
+    """
     if value == "BACK":
         entry.delete(len(entry.get()) - 1, END)
         invoer.pop()
@@ -22,25 +26,41 @@ def select(value):
     elif value == " Tab ":
         entry.insert(END, '    ')
     else:
-        # entry.insert(tkinter.END, value)
         entry.insert(END, value)
         invoer.append(value)
         print(invoer)
 
 
 def check_kenteken():
-    # checkt kenteken
-    kenteken = "aaa"
-    # if kenteken == "NOTALLOWED":  # Check if the car release is after 2001 AND uses a Diesel engine
-    #     return not_allowed_screen()
+    # TODO: check of kenteken via Image Text Recognition gevonden kan worden
+    """"
+    Check of de Image Text Recognition een kenteken heeft gevonden
+    """
+    global kenteken
+    kenteken = "NOTALLOWED"
     if len(kenteken) == 0:  # Geen kenteken gevonden
         return not_found_screen()
     else:
         return found_screen(kenteken)  # Kenteken gevonden
 
 
+def check_rdw():
+    # TODO: check of kenteken toegestaan is mbv RDW API
+    """"
+    Check of de kenteken toegestaan is
+    """
+    if kenteken == "NOTALLOWED":  # Check if the car release is after 2001 AND uses a Diesel engine
+        return not_allowed_screen()
+    else:
+        return entry_screen()
+
+
 def start_screen():
-    # Start scherm ('Welkom', BTN: Start)
+    """"
+    scherm: Start scherm
+    String: Welkom
+    Button: Start
+    """""
     start = Tk()
     start.title('')
     start.geometry('550x300')
@@ -56,30 +76,56 @@ def start_screen():
 
 
 def check_screen():
-    # Kenteken check scherm ('Kenteken scannen...')
+    """"
+    scherm: Kenteken checken
+    String: Kenteken scannen...
+    Sleep: 8 seconden
+    """""
     checkscreen = Tk()
     checkscreen.title('')
     checkscreen.geometry('550x300')
     checkscreen.configure(background='white')
+
+    def next():
+        checkscreen.destroy()
+        check_kenteken()
+
     w = Label(checkscreen, text="Kenteken scannen...", font=("Arial", 20), background="white")
     w.pack(side='top')
-    w.after(2000, lambda: checkscreen.destroy())
+    w.after(2000, lambda: next())
     mainloop()
 
 
 def not_allowed_screen():
-    # Kenteken niet toegestaan ('Uw auto is niet toegestaan')
+    """"
+    Scherm: Kenteken niet toegestaan
+    String: Helaas, uw voertuig voldoent niet aan de eisen...
+    Sleep: 8 seconden
+    """
     notallowed = Tk()
     notallowed.title('')
     notallowed.geometry('550x300')
     notallowed.configure(background='white')
-    btn = Button(notallowed, text='NOT ALLOWED', height=6, width=20, font=("Arial", 20), command=notallowed.destroy)
-    btn.place(relx=.5, rely=.5, anchor="c")
+    w = Label(notallowed, text="Helaas, uw voertuig voldoet niet aan de", font=("Arial", 20), background="white")
+    w2 = Label(notallowed, text="eisen om in het centrum van Utrecht te", font=("Arial", 20), background="white")
+    w3 = Label(notallowed, text="rijden. Hierom mogen wij u geen toegang", font=("Arial", 20), background="white")
+    w4 = Label(notallowed, text="verlenen aan Parkeergarage de Kruisstraat", font=("Arial", 20), background="white")
+    w.place(x=10, y=50, anchor="w")
+    w2.place(x=10, y=100, anchor="w")
+    w3.place(x=10, y=150, anchor="w")
+    w4.place(x=10, y=200, anchor="w")
+    w.after(8000, lambda: notallowed.destroy())
     mainloop()
 
 
 def found_screen(kenteken):
-    # Kenteken gevonden ('Kenteken: XX-XX-XX' 'Is dit juist?', BTN: JA. BTN: Handmatig)
+    """"
+    Scherm: Kenteken gevonden
+    String: Kenteken: XX-XX-XX
+            is dit juist?
+    Button: Ja
+            Handmatig
+    """
     found = Tk()
     found.title('')
     found.geometry('550x300')
@@ -89,39 +135,63 @@ def found_screen(kenteken):
     k = Label(found, text='Is dit juist?', font=("Arial", 20), background="white")
     k.pack(side='top')
 
-    global test2
-    test2 = 0
-
     def handmatig():
-        global test2
-        test2 = 1
         found.destroy()
         handmatig_screen()
 
-    btn = Button(found, text='Ja', height=3, width=15, font=("Arial", 20), command=found.destroy)
+    def entry():
+        found.destroy()
+        check_rdw()
+
+    btn = Button(found, text='Ja', height=3, width=15, font=("Arial", 20), command=entry)
     btn2 = Button(found, text='Handmatig', height=3, width=15, font=("Arial", 20), command=handmatig)
-    btn.place(x=200, y=350, anchor="c")
-    btn2.place(x=600, y=350, anchor="c")
+    btn.place(x=140, y=200, anchor="c")
+    btn2.place(x=410, y=200, anchor="c")
     mainloop()
 
 
 def not_found_screen():
-    # Kenteken niet gevonden ('Kenteken niet gevonden', BTN: Opnieuw, BTN: Handmatig)
+    """"
+    Scherm: Kenteken NIET gevonden
+    String: Kenteken niet gevonden
+    Button: Opnieuw
+            Handmatig
+    """
     denied = Tk()
     denied.title('')
     denied.geometry('550x300')
     denied.configure(background='white')
-    btn = Button(denied, text='NOT FOUND', height=6, width=20, font=("Arial", 20), command=denied.destroy)
-    btn.place(relx=.5, rely=.5, anchor="c")
+    w = Label(denied, text="Kenteken niet gevonden", font=("Arial", 20), background="white")
+    w.pack(side='top')
+
+    def handmatig():
+        denied.destroy()
+        handmatig_screen()
+
+    def opnieuw():
+        denied.destroy()
+        check_screen()
+
+    btn = Button(denied, text='Opnieuw', height=3, width=15, font=("Arial", 20), command=opnieuw)
+    btn2 = Button(denied, text='handmatig', height=3, width=15, font=("Arial", 20), command=handmatig)
+    btn.place(x=140, y=200, anchor="c")
+    btn2.place(x=410, y=200, anchor="c")
     mainloop()
 
 
 def handmatig_screen():
+    """"
+    Scherm: Handmatig kenteken invoeren
+    String: Kenteken:
+    Button: Keyboard layout
+            Annuleren
+            Bevestigen
+    """
     kb = Tk()
     kb.title("HosoKeys")
     kb.geometry('550x300')
     kb.resizable(0, 0)
-    #kb.columnconfigure(0, minsize=230)
+    kb.configure(background='white')
     kb.rowconfigure(0, minsize=80)
     kb.rowconfigure(2, minsize=20)
     global entry
@@ -155,13 +225,40 @@ def handmatig_screen():
         if varColumn > 14 and varRow == 5:
             varColumn = 0
             varRow += 1
+
+    def next():
+        kenteken = ''.join(invoer)
+        print(kenteken)
+        kb.destroy()
+        check_rdw()
+
+    w = Label(kb, text="Kenteken:", font=("Arial", 20), background="white")
+    w.place(x=270, y=50, anchor="c")
+    test2 = Button(kb, text='Annuleren', height=1, width=15, font=("Arial", 20), command=kb.destroy)
+    test3 = Button(kb, text='Bevestigen', height=1, width=15, font=("Arial", 20), command=next)
+    test2.place(x=140, y=250, anchor="c")
+    test3.place(x=410, y=250, anchor="c")
     kb.mainloop()
+
+
+def entry_screen():
+    """"
+    Scherm: Entry scherm
+    String: Kom binnen
+    Sleep: 8 seconden
+    """
+    entryscreen = Tk()
+    entryscreen.title('')
+    entryscreen.geometry('550x300')
+    entryscreen.configure(background='white')
+    w = Label(entryscreen, text="Kom binnen", font=("Arial", 20), background="white")
+    w.pack(side='top')
+    w.after(8000, lambda: entryscreen.destroy())
+    mainloop()
 
 
 while True:
     start_screen()
     check_screen()
-
     check_kenteken()
-    print(test2)
-    # handmatig_screen()
+    # TODO: variabele kenteken moet in de database worden geplaatst + check of hij aan eerder account gekoppeld is
